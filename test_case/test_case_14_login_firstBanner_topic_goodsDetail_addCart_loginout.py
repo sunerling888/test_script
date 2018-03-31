6466d7ed4be9c062f07cf616b5f73e3c9d9dcb01
love112728@gmail.com <YOUR EMAIL ADDRESS>
2018-03-31 18:00:17 +0800

    新增14py文件

--- test_case/test_case_14_login_firstBanner_topic_goodsDetail_addCart_loginout.py
+++ test_case/test_case_14_login_firstBanner_topic_goodsDetail_addCart_loginout.py
@@ -28,7 +28,7 @@ class firstBanner_addCart(unittest.TestCase):
     '''获取test_user.csv文件'''
     users = ReaderCsv('/Users/dabenchen/Downloads/daben_chen_py/test_data/test_user.csv')
     # users = ReaderCsv('/home/xiaoqiang.li/scripts/release_li/test_api_dvd/test_data/test_user.csv')
-
+    topics = ReaderCsv('/Users/dabenchen/Downloads/daben_chen_py/test_data/test_topic.csv')
 
     # =====执行测试======
     def setUp(self):
@@ -45,8 +45,8 @@ class firstBanner_addCart(unittest.TestCase):
 
     # ==============测试case===================
     '''以下是: 登录 -> 首页banner -> 专题页 -> [随机专题页商品id] -> 商品详情页 -> 加入购物车(效验商品id,数量) -> 退出登录【卖家/买家/游客】'''
-    def action_firstBanner_addCart(self, topic_Id = None):
-
+    def action_firstBanner_addCart(self, topicId=None):
+        '''
         # 请求首页第一屏
         print u'首页第一屏'
         data = self.session.api('/api/mg/sale/index/getPageFirst')
@@ -75,20 +75,42 @@ class firstBanner_addCart(unittest.TestCase):
         randIndex = random.randint(0, length - 1)
         topicId = contents[randIndex]
         print "DEBUG\t[%s][%s]" % (u'topicId', topicId)
+        '''
 
-
-
+        
         # 请求首页banner专题页
         print u'首页banner专题'
-        param = {'topicId': topicId}
-        data = self.session.api('/api/mg/sale/topic/getTopicInfo', param)
+        print topicId
+        param = {'topicId': topicId['topicId']}
+        data = self.session.api('/api/mg/sale/topic/getTopicInfo?15224819553866', param)
         self.assertEqual(int(data['code']), 0, data['data'])
-        print json.dumps(data)
+        
+        #print json.dumps(data)
+        print type(data)
 
-        # 获取goods
-        goods = 
+        
 
-    
+        topicContent = data['data']['topicContent']
+        # print type(topicContent)
+        # 返回的是字符串，先loads转换成对象
+        topicContent = json.loads(topicContent)
+        # print type(topicContent)
+        # 取下标第4个
+        # print topicContent['list'][4]
+        
+        goodsIds = topicContent['list'][4]['goods']
+        print goodsIds
+        '''
+        goodsIds = []
+        for item in topicContent:
+            if 'list' not in item :
+                continue
+            goodsIds.append((int)(item['list'][4]['goods']))
+
+        print goodsIds
+        '''
+        return True
+        
 
     # =============执行case===============
     def test_01_seller_firstBanner_addCart(self):
@@ -96,5 +118,6 @@ class firstBanner_addCart(unittest.TestCase):
         user = self.users.next()
         self.session.api('/api/mg/auth/user/login', user)
 
-        ret = self.action_firstBanner_addCart()
+        topicId = self.topics.random()
+        ret = self.action_firstBanner_addCart(topicId)
         self.assertTrue(ret)
