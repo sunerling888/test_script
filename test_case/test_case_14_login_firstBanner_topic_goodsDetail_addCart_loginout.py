3952858c49aedb87bda5fa4ae29ea4ac8bee6266
love112728@gmail.com <YOUR EMAIL ADDRESS>
2018-04-02 14:55:36 +0800

    新增14.py

--- test_case/test_case_14_login_firstBanner_topic_goodsDetail_addCart_loginout.py
+++ test_case/test_case_14_login_firstBanner_topic_goodsDetail_addCart_loginout.py
@@ -45,79 +45,109 @@ class firstBanner_addCart(unittest.TestCase):
 
     # ==============测试case===================
     '''以下是: 登录 -> 首页banner -> 专题页 -> [随机专题页商品id] -> 商品详情页 -> 加入购物车(效验商品id,数量) -> 退出登录【卖家/买家/游客】'''
-    def action_firstBanner_addCart(self, topicId=None):
-        '''
-        # 请求首页第一屏
-        print u'首页第一屏'
-        data = self.session.api('/api/mg/sale/index/getPageFirst')
-
-        feedList = data['data']['feedList']
-        contents = []
-        for item in feedList:
-            if not item.has_key('body') or not item['body'].has_key('dataList'):
-                continue
-            if len(item['body']['dataList']) < 0:
-                continue
-
-            dataList = item['body']['dataList']
-            for data in dataList:
-                if not data.has_key('command'):
-                    continue
-                m = re.match(r'^/.+\?topicId=(\d+)&', data['command']['content'])
-                if m:
-                    topic_Id = m.group(1)
-                    contents.append(topic_Id)
-        print "DEBUG\t[%s][%s]" % (u'contents', contents)
-
-        length = len(contents)
-        print "DEBUG\t[%s][%s]" % (u'length', length)
-
-        randIndex = random.randint(0, length - 1)
-        topicId = contents[randIndex]
-        print "DEBUG\t[%s][%s]" % (u'topicId', topicId)
-        '''
-
-        
+    def action_firstBanner_addCart(self, topic_Id=None):
+          
         # 请求首页banner专题页
         print u'首页banner专题'
-        print topicId
-        param = {'topicId': topicId['topicId']}
-        data = self.session.api('/api/mg/sale/topic/getTopicInfo?15224819553866', param)
+        print topic_Id
+        param = {'topicId': topic_Id['topicId']}
+        data = self.session.api('/api/mg/sale/topic/getTopicInfo', param)
         self.assertEqual(int(data['code']), 0, data['data'])
         
-        #print json.dumps(data)
-        print type(data)
+        # print json.dumps(data)
+        # print type(data)
 
         
-
+        # 取出专题页goodsId
         topicContent = data['data']['topicContent']
         # print type(topicContent)
         # 返回的是字符串，先loads转换成对象
         topicContent = json.loads(topicContent)
+        print type(topicContent)
         # print type(topicContent)
         # 取下标第4个
         # print topicContent['list'][4]
-        
-        goodsIds = topicContent['list'][4]['goods']
-        print goodsIds
-        '''
         goodsIds = []
-        for item in topicContent:
-            if 'list' not in item :
-                continue
-            goodsIds.append((int)(item['list'][4]['goods']))
-
+        goods_Ids = topicContent['list'][4]['goods']
+        # 转换成数组list
+        goodsIds = goods_Ids.split(',')
         print goodsIds
-        '''
+
+        # 随机list
+        length = len(goodsIds)
+        randIndex = random.randint(0, length - 1)
+        goodsId = goodsIds[randIndex]
+        print goodsId
+        
+        # 请求商品详情页
+        print u'商品详情页'
+        print "DEBUG\t[%s][%s]" % (u'goodsId', goodsId)
+        param = {'goodsId': goodsId}
+        data = self.session.api('/api/mg/good/info/detail', param)
+        self.assertEqual(int(data['code']), 0, data['data'])
+        
+        
+        # 添加购物车
+        print u'添加购物车'
+        # 添加购物车的接口，需要goods: {"number":1,"goods_id":"550358"},用urllib.quote的方式
+        param = {'m': 'default', 'c': 'cart', 'a': 'add_to_cart', 'goods': urllib.quote(json.dumps({"number":1, "goods_id":goodsId}))}
+        print param['goods']
+        data = self.session.post('/cart.html', urllib.urlencode(param))
+        print data
+
+
+        # 请求购物车页面
+        print u'购物车页面'
+        response = self.session.get('/cart.html')
+        # 请求购物车接口
+        print u'购物车接口'
+        response = self.session.get('/cart.html?c=cart&a=load')
+        # print response
+
+        data = json.loads(response['body'])
+
+        cart_info = data['data']['cart_info']
+        goodsIds = {}
+        activitys = cart_info['activitys']
+        for item in activitys:
+            if 'goods' not in item:
+                continue
+
+            for goods in item["goods"]:
+                if 'goods_id' not in goods:
+                    continue
+                goods_number = int(goods["goods_number"])
+                goodsIds[int(goods['goods_id'])] = goods_number
+                # goodsIds.append((int)(goods['goods_id']))
+        # print goodsId
+        # print goodsIds
+        # self.assertTrue(goodsId in goodsIds.keys(), u"商品不在购物车中")
         return True
         
 
     # =============执行case===============
     def test_01_seller_firstBanner_addCart(self):
-        print u'卖家身份:'
+        print u'卖家身份: 新专题 -> 【随机选择商品id】-> 商品详情页 -> 加入购物车 -> 效验购物车 -> 退出登录'
         user = self.users.next()
         self.session.api('/api/mg/auth/user/login', user)
 
-        topicId = self.topics.random()
-        ret = self.action_firstBanner_addCart(topicId)
+        topic_Id = self.topics.random()
+        ret = self.action_firstBanner_addCart(topic_Id)
+        self.assertTrue(ret)
+
+
+    def test_02_user_firstBanner_addCart(self):
+        print u'买家身份: 新专题 -> 【随机选择商品id】-> 商品详情页 -> 加入购物车 -> 效验购物车 -> 退出登录'
+        user = self.users.next()
+        self.session.api('/api/mg/auth/user/login', user)
+
+        topic_Id = self.topics.random()
+        ret = self.action_firstBanner_addCart(topic_Id)
+        self.assertTrue(ret)
+
+    
+    def test_03_no_firstBanner_addCart(self):
+        print u'游客身份: 新专题 -> 【随机选择商品id】-> 商品详情页 -> 加入购物车 -> 效验购物车 -> 退出登录'
+        topic_Id = self.topics.random()
+        ret = self.action_firstBanner_addCart(topic_Id)
         self.assertTrue(ret)
