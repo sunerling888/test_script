53cc43b2c69b08b7e5fc91824696047035942ae3
love112728@gmail.com <YOUR EMAIL ADDRESS>
2018-04-03 11:48:46 +0800

    update 15.py

--- test_case/test_case_15_searchGoods_goodsDetail_getCart_loginout.py
+++ test_case/test_case_15_searchGoods_goodsDetail_getCart_loginout.py
@@ -43,25 +43,47 @@ class searchgoods_getCart(unittest.TestCase):
     # ===================测试case====================
     def action_goods_getCart(self, goods_Id=None):
 
-        '''
+        
         # 请求分类页(图书0-2岁)
         print u"分类页(图书0-2岁)"
         response = self.session.get('/categorySync-8-14-2.html?sort=2&_t=1522657956463.7556&page_size=10&page=1')
         # data = json.loads(response['body'])
         print type(response)
-        print response
-        '''
+        # print response
         
+
+        
+        '''
         # 请求商品详情页
         print u'商品详情页'
         param = {'goodsId':goods_Id['goodsId']}
         data = self.session.api('/api/mg/good/info/detail', param)
         self.assertEqual(int(data['code']), 0, data['data'])
         print json.dumps(data)
-
-        # 加入购物车
-        print u'加入购物车'
         
+        ''' 
+        # print response['body']
+        
+        # 取出good_id,随机goods_id
+        body = response['body']
+        
+        body = json.loads(body)
+        # print body
+        goodIds = []
+        # if data not in body['data']:
+            # return False
+        if not body.has_key('data'):    # 判断body中的key:data
+            return False
+        for item in body['data']:       # 遍历key:data
+            # if 'data' not in item:
+                # continue
+            # if len(item['data']) < 1 or 'goods_id' not in item['data'][0]:
+                # continue
+            # print item['data'][0]['goods_id']
+            goodIds.append(int(item['goods_id']))
+        print goodIds
+        # goodIds = body['data'][0]['goods_id']
+        # print type(goodIds)
         return True
 
 
