c957e6c5bede2ee4ee6cdb53b91df7dc7201996a
love112728@gmail.com <YOUR EMAIL ADDRESS>
2018-04-14 14:06:21 +0800

    1.新增21.py

---
+++ test_case/test_case_21_login_search_buyGoods_orderpay_loginout.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env Python
+# -*- coding:utf-8 -*-
+
+'''
+test_case_21_login_search_buyGoods_orderpay_loginout.py
+# 登录 -> 搜索商品 -> 商品详情页 -> 购买 -> 订单确认 -> 支付完成
+'''
+
+import requests
+import urllib,urllib2
+import 
